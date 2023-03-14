0b0af50d0d5fb1ef013084377b6026b1dbbb0032
kipngeno kevin <kipngenokevin254@gmail.com>
2023-03-14 20:40:39 +0300

    Day 1

--- src/Components/Brands/Brand.jsx
+++ src/Components/Brands/Brand.jsx
@@ -1,17 +1,159 @@
 import React from 'react'
+import './Brand.scss'
 
 const Brand = (props) => {
-    const { open } = props;
+  const { open } = props;
   return (
     <>
-     {open && (
-        <div className="container">
-           Brand Component
+      {open && (
+        <div className="brand">
+          <div className='brand_category'>
+            <span className='brand_name'>Botega</span>
+            <ul className='brand_list'>
+              <li className='subcategory_name'>Arco Bag</li>
+              <li className='subcategory_name'>Arco Bag</li>
+              <li className='subcategory_name'>Arco Bag</li>
+              <li className='subcategory_name'>Arco Bag</li>
+              <li className='subcategory_name'>Arco Bag</li>
+            </ul>
+            </div>
+
+            <div className='brand_category'>
+            <span className='brand_name'>Botega</span>
+            <ul className='brand_list'>
+              <li className='subcategory_name'>Arco Bag</li>
+              <li className='subcategory_name'>Arco Bag</li>
+              <li className='subcategory_name'>Arco Bag</li>
+              <li className='subcategory_name'>Arco Bag</li>
+              <li className='subcategory_name'>Arco Bag</li>
+            </ul>
+
+            </div>
+           <div className='brand_category'>
+            <span className='brand_name'>Botega</span>
+            <ul className='brand_list'>
+              <li className='subcategory_name'>Arco Bag</li>
+              <li className='subcategory_name'>Arco Bag</li>
+              <li className='subcategory_name'>Arco Bag</li>
+              <li className='subcategory_name'>Arco Bag</li>
+              <li className='subcategory_name'>Arco Bag</li>
+            </ul>
+          </div>
+          <div className='brand_category'>
+            <span className='brand_name'>Botega</span>
+            <ul className='brand_list'>
+              <li className='subcategory_name'>Arco Bag</li>
+              <li className='subcategory_name'>Arco Bag</li>
+              <li className='subcategory_name'>Arco Bag</li>
+              <li className='subcategory_name'>Arco Bag</li>
+              <li className='subcategory_name'>Arco Bag</li>
+            </ul>
+          </div>
+          <div className='brand_category'>
+            <span className='brand_name'>Botega</span>
+            <ul className='brand_list'>
+              <li className='subcategory_name'>Arco Bag</li>
+              <li className='subcategory_name'>Arco Bag</li>
+              <li className='subcategory_name'>Arco Bag</li>
+              <li className='subcategory_name'>Arco Bag</li>
+              <li className='subcategory_name'>Arco Bag</li>
+            </ul>
+          </div>
+          <div className='brand_category'>
+            <span className='brand_name'>Botega</span>
+            <ul className='brand_list'>
+              <li className='subcategory_name'>Arco Bag</li>
+              <li className='subcategory_name'>Arco Bag</li>
+              <li className='subcategory_name'>Arco Bag</li>
+              <li className='subcategory_name'>Arco Bag</li>
+              <li className='subcategory_name'>Arco Bag</li>
+            </ul>
+          </div>
+          <div className='brand_category'>
+            <span className='brand_name'>Botega</span>
+            <ul className='brand_list'>
+              <li className='subcategory_name'>Arco Bag</li>
+              <li className='subcategory_name'>Arco Bag</li>
+              <li className='subcategory_name'>Arco Bag</li>
+              <li className='subcategory_name'>Arco Bag</li>
+              <li className='subcategory_name'>Arco Bag</li>
+            </ul>
+          </div>
+          <div className='brand_category'>
+            <span className='brand_name'>Botega</span>
+            <ul className='brand_list'>
+              <li className='subcategory_name'>Arco Bag</li>
+              <li className='subcategory_name'>Arco Bag</li>
+              <li className='subcategory_name'>Arco Bag</li>
+              <li className='subcategory_name'>Arco Bag</li>
+              <li className='subcategory_name'>Arco Bag</li>
+            </ul>
+          </div>
+          <div className='brand_category'>
+            <span className='brand_name'>Botega</span>
+            <ul className='brand_list'>
+              <li className='subcategory_name'>Arco Bag</li>
+              <li className='subcategory_name'>Arco Bag</li>
+              <li className='subcategory_name'>Arco Bag</li>
+              <li className='subcategory_name'>Arco Bag</li>
+              <li className='subcategory_name'>Arco Bag</li>
+            </ul>
+          </div>
+          <div className='brand_category'>
+            <span className='brand_name'>Botega</span>
+            <ul className='brand_list'>
+              <li className='subcategory_name'>Arco Bag</li>
+              <li className='subcategory_name'>Arco Bag</li>
+              <li className='subcategory_name'>Arco Bag</li>
+              <li className='subcategory_name'>Arco Bag</li>
+              <li className='subcategory_name'>Arco Bag</li>
+            </ul>
+          </div>
+          <div className='brand_category'>
+            <span className='brand_name'>Botega</span>
+            <ul className='brand_list'>
+              <li className='subcategory_name'>Arco Bag</li>
+              <li className='subcategory_name'>Arco Bag</li>
+              <li className='subcategory_name'>Arco Bag</li>
+              <li className='subcategory_name'>Arco Bag</li>
+              <li className='subcategory_name'>Arco Bag</li>
+            </ul>
+          </div>
+          <div className='brand_category'>
+            <span className='brand_name'>Botega</span>
+            <ul className='brand_list'>
+              <li className='subcategory_name'>Arco Bag</li>
+              <li className='subcategory_name'>Arco Bag</li>
+              <li className='subcategory_name'>Arco Bag</li>
+              <li className='subcategory_name'>Arco Bag</li>
+              <li className='subcategory_name'>Arco Bag</li>
+            </ul>
+          </div>
+          <div className='brand_category'>
+            <span className='brand_name'>Botega</span>
+            <ul className='brand_list'>
+              <li className='subcategory_name'>Arco Bag</li>
+              <li className='subcategory_name'>Arco Bag</li>
+              <li className='subcategory_name'>Arco Bag</li>
+              <li className='subcategory_name'>Arco Bag</li>
+              <li className='subcategory_name'>Arco Bag</li>
+            </ul>
+          </div>
+          <div className='brand_category'>
+            <span className='brand_name'>Botega</span>
+            <ul className='brand_list'>
+              <li className='subcategory_name'>Arco Bag</li>
+              <li className='subcategory_name'>Arco Bag</li>
+              <li className='subcategory_name'>Arco Bag</li>
+              <li className='subcategory_name'>Arco Bag</li>
+              <li className='subcategory_name'>Arco Bag</li>
+            </ul>
+          </div>
         </div>
       )}
 
     </>
-   
+
   )
 }
 
